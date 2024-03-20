4aa6447e64cb944b740023de1b8356dffec8ea3a
Ludoviko451 <luisangelquirosflorez@gmail.com>
2024-03-20 13:17:59 -0500

    Carpetas base del proyecto

---
+++ src/main/java/com/bootcamp2024/bootcamp2024/Bootcamp2024Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp2024.bootcamp2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp2024/bootcamp2024/Bootcamp2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp2024.bootcamp2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
